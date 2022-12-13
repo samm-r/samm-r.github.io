280cd52e407c03d376d49ffebac896e6f8e60705
sam r <120363386+samm-r@users.noreply.github.com>
2022-12-13 21:23:13 +1300

    Update throw.js
    thanks to shorturl.at/mEGMP

--- throw/throw.js
+++ throw/throw.js
@@ -1,1 +1,32 @@
+/* URL in address bar: 
+   http://localhost/test/js-url-parameters/#/str1/str2/str3/
+*/
 
+var docURL = window.location.href,
+  params = [];
+
+// filter out the website origin "example.github.io" in the OP example      
+docURL = docURL.replace(window.location.origin, '');
+
+// if /#/ found then we have URL parameters
+// grabbing the parameters part of the URL
+if (docURL.indexOf('/#/') > -1) {
+  docURL = docURL.split('/#/')[1];
+  if (docURL != '') {
+
+    // omit the last forward slash if exist
+    if (docURL[docURL.length - 1] == '/') {
+      docURL = docURL.substring(0, docURL.length - 1);
+    }
+
+    // split the URL final string o get an object with all params 
+    params = docURL.split('/');
+    console.log(params);
+  }
+} else {
+  console.log('No URL parameters found');
+}
+
+/* Output: 
+   ["str1", "str2", "str3"]
+*/

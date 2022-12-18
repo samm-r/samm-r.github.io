acef7d2e5a34239276f4315bb4b60c88a96190dc
sam r <120363386+samm-r@users.noreply.github.com>
2022-12-18 23:56:04 +1300

    Update throw.js

--- throw/throw.js
+++ throw/throw.js
@@ -1,32 +1,18 @@
-/* URL in address bar: 
-   http://localhost/test/js-url-parameters/#/str1/str2/str3/
-*/
 var docURL = window.location.href,
-  params = [];
-
-// filter out the website origin "example.github.io" in the OP example      
+  params = [];     
 docURL = docURL.replace(window.location.origin, '');
 
-// if /#/ found then we have URL parameters
-// grabbing the parameters part of the URL
 if (docURL.indexOf('/#/') > -1) {
   docURL = docURL.split('/#/')[1];
   if (docURL != '') {
 
-    // omit the last forward slash if exist
     if (docURL[docURL.length - 1] == '/') {
       docURL = docURL.substring(0, docURL.length - 1);
     }
 
-    // split the URL final string o get an object with all params 
     params = docURL.split('/');
     console.log(params);
   }
 } else {
-  console.log('No URL parameters found');
+    document.getElementById("main").innerHTML= "no params found";
 }
-
-    // uthorization: "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"
-/* Output: 
-   ["str1", "str2", "str3"]
-*/

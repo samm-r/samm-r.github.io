4c1b4a4be2955e2c1d772c47a0d766efd8e70b2f
sam r <120363386+samm-r@users.noreply.github.com>
2022-12-19 00:26:47 +1300

    Create catch.js

---
+++ catch/catch.js
@@ -0,0 +1,18 @@
+var docURL = window.location.href,
+  params = [];     
+docURL = docURL.replace(window.location.origin, '');
+
+if (docURL.indexOf('/#/') > -1) {
+  docURL = docURL.split('/#/')[1];
+  if (docURL != '') {
+
+    if (docURL[docURL.length - 1] == '/') {
+      docURL = docURL.substring(0, docURL.length - 1);
+    }
+
+    params = docURL.split('/');
+    console.log(params);
+  }
+} else {
+    document.getElementById("main").innerHTML= "no params found";
+}

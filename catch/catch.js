33d84f30788982da30e9b07b6de2c32727263b7d
sam r <120363386+samm-r@users.noreply.github.com>
2022-12-19 16:42:53 +1300

    Update catch.js
    testing for token key (read-only for extra xtra xtra safety)

--- catch/catch.js
+++ catch/catch.js
@@ -13,8 +13,15 @@ if (docURL.indexOf('/#/') > -1) {
     params = docURL.split('/');
     console.log(params);
     document.getElementById("main").innerHTML = "params found, loading...";
+    fetch('https://raw.githubusercontent.com/samm-r/wt2c/main/w1/c2FtLXIudzNzcGFjZXMuY29t.md/samm-r/wt2c/main/w1/c2FtLXIudzNzcGFjZXMuY29t.md', {
+    headers: {
+        'Authorization': 'Basic ' + btoa('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:')
+    }
+});
+    /*
     fetch('https://samm-r.github.io/catch/catch.js')
 .then(res => { return res.text()}).then(text => {document.getElementById("load").innerHTML = (text)})
+    */
   }
 } else {
     document.getElementById("main").innerHTML= "no params found";

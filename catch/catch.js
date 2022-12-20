793b91e58feddb6f94c7e3d484217a3a34742f8a
sam r <120363386+samm-r@users.noreply.github.com>
2022-12-20 23:38:20 +1300

    Update catch.js

--- catch/catch.js
+++ catch/catch.js
@@ -13,6 +13,7 @@ if (docURL.indexOf('/#/') > -1) {
     params = docURL.split('/');
     console.log(params);
     document.getElementById("main").innerHTML = "params found, loading...";
+    document.getElementById("small").innerHTML = params;
     fetch('https://raw.githubusercontent.com/samm-r/wt2c/main/w1/c2FtLXIudzNzcGFjZXMuY29t.md', {
     headers: {
         'Authorization': 'Basic ' + btoa('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:')
@@ -25,4 +26,5 @@ if (docURL.indexOf('/#/') > -1) {
   }
 } else {
     document.getElementById("main").innerHTML= "no params found";
+  document.getElementById("small").innerHTML = "_";
 }
